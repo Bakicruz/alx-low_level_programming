3a1489f8b513e21f959986c03a30342b79af1640
Bakicruz <bakitizazu@gmail.com>
2022-06-05 06:02:44 -0700

    Sizing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main function - it is void
+ * main - it is void
+ * return - main 0
  */
-
-int main(void)
+int main(void)/** main - void
+		* return - 0
+		*/
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
